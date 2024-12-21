a6b83219ca74cb6bf91f5260399dfca5cf3bbdc4
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2024-12-21 19:58:35 +0200

    remove task schedulled autostart to use only registry way

--- OpenHardwareMonitor/UI/StartupManager.cs
+++ OpenHardwareMonitor/UI/StartupManager.cs
@@ -63,17 +63,20 @@ namespace OpenHardwareMonitor.UI
 
                         ITaskFolder folder = _scheduler.GetFolder("\\Open Hardware Monitor");
                         IRegisteredTask task = folder.GetTask("Startup");
-                        _startup = (task != null) &&
-                          (task.Definition.Triggers.Count > 0) &&
-                          (task.Definition.Triggers[1].Type ==
-                            TASK_TRIGGER_TYPE2.TASK_TRIGGER_LOGON) &&
-                          (task.Definition.Actions.Count > 0) &&
-                          (task.Definition.Actions[1].Type ==
-                            TASK_ACTION_TYPE.TASK_ACTION_EXEC) &&
-                          (task.Definition.Actions[1] as IExecAction != null) &&
-                          ((task.Definition.Actions[1] as IExecAction).Path ==
-                            Application.ExecutablePath);
+                        _startup = task != null &&
+                          task.Definition.Triggers.Count > 0 &&
+                          task.Definition.Triggers[1].Type == TASK_TRIGGER_TYPE2.TASK_TRIGGER_LOGON &&
+                          task.Definition.Actions.Count > 0 &&
+                          task.Definition.Actions[1].Type == TASK_ACTION_TYPE.TASK_ACTION_EXEC &&
+                          task.Definition.Actions[1] is IExecAction execAction &&
+                          execAction.Path == Application.ExecutablePath;
 
+                        if (_startup)
+                        {
+                            //old versions compatibility - convert task to registry
+                            DeleteSchedulerTask();
+                            CreateRegistryRun();
+                        }
                     }
                     catch (IOException)
                     {
@@ -126,37 +129,6 @@ namespace OpenHardwareMonitor.UI
             }
         }
 
-        private void CreateSchedulerTask()
-        {
-            ITaskDefinition definition = _scheduler.NewTask(0);
-            definition.RegistrationInfo.Description =
-              "This task starts the Open Hardware Monitor on Windows startup.";
-            definition.Principal.RunLevel =
-              TASK_RUNLEVEL.TASK_RUNLEVEL_HIGHEST;
-            definition.Settings.DisallowStartIfOnBatteries = false;
-            definition.Settings.StopIfGoingOnBatteries = false;
-            definition.Settings.ExecutionTimeLimit = "PT0S";
-            _ = (ILogonTrigger)definition.Triggers.Create(TASK_TRIGGER_TYPE2.TASK_TRIGGER_LOGON);
-            IExecAction action = (IExecAction)definition.Actions.Create(TASK_ACTION_TYPE.TASK_ACTION_EXEC);
-            action.Path = Application.ExecutablePath;
-            action.WorkingDirectory =
-              Path.GetDirectoryName(Application.ExecutablePath);
-
-            ITaskFolder root = _scheduler.GetFolder("\\");
-            ITaskFolder folder;
-            try
-            {
-                folder = root.GetFolder("Open Hardware Monitor");
-            }
-            catch (IOException)
-            {
-                folder = root.CreateFolder("Open Hardware Monitor", "");
-            }
-            folder.RegisterTaskDefinition("Startup", definition,
-              (int)TASK_CREATION.TASK_CREATE_OR_UPDATE, null, null,
-              TASK_LOGON_TYPE.TASK_LOGON_INTERACTIVE_TOKEN, "");
-        }
-
         private void DeleteSchedulerTask()
         {
             ITaskFolder root = _scheduler.GetFolder("\\");
@@ -195,38 +167,23 @@ namespace OpenHardwareMonitor.UI
             }
             set
             {
-                if (_startup != value)
+                if (_startup == value)
+                    return;
+
+                if (!IsAvailable)
+                    throw new InvalidOperationException();
+
+                try
                 {
-                    if (IsAvailable)
-                    {
-                        if (_scheduler != null)
-                        {
-                            if (value)
-                                CreateSchedulerTask();
-                            else
-                                DeleteSchedulerTask();
-                            _startup = value;
-                        }
-                        else
-                        {
-                            try
-                            {
-                                if (value)
-                                    CreateRegistryRun();
-                                else
-                                    DeleteRegistryRun();
-                                _startup = value;
-                            }
-                            catch (UnauthorizedAccessException)
-                            {
-                                throw new InvalidOperationException();
-                            }
-                        }
-                    }
+                    if (value)
+                        CreateRegistryRun();
                     else
-                    {
-                        throw new InvalidOperationException();
-                    }
+                        DeleteRegistryRun();
+                    _startup = value;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    throw new InvalidOperationException();
                 }
             }
         }
