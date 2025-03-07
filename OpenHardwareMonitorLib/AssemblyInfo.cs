53eee61698ba8d2bcaf06b5c631b76df872fec75
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-03-07 15:53:49 +0200

    OpenHardwareMonitorLib upgraded to netstandard2.0

--- OpenHardwareMonitorLib/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Open Hardware Monitor Lib")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sergiy Egoshyn")]
-[assembly: AssemblyProduct("Open Hardware Monitor")]
-[assembly: AssemblyCopyright("Copyright © 2022 Sergiy Egoshyn")]
-[assembly: AssemblyVersion("1.0.*")]
