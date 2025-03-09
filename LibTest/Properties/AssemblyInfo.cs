3bf09354298e8605d26bb9138aaaf7cfc132c1d1
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-03-09 10:30:36 +0200

    add nuget package test console app

---
+++ LibTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a8b894d-b19c-41e5-bf7b-3b95c8746e52")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
